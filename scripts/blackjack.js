53e26f35c691a9e319ea48f9804436d694246f7c
Jeff Cook <jecook2692@gmail.com>
2020-04-23 22:50:39 -0400

    adds css

---
+++ scripts/blackjack.js
@@ -0,0 +1,21 @@
+const table = document.getElementById("table");
+table.style.color = "rgb(31,113,198)";
+table.style.backgroundColor = "green";
+table.style.display = "flex";
+table.style.flexDirection = "column";
+table.style.alignItems ="center";
+table.style.marginTop = "180px";
+table.style.marginLeft = "100px"
+table.style.width = "80vw";
+table.style.height = "30vw";
+table.style.borderRadius = "80px";
+table.style.borderStyle = "solid";
+table.style.borderColor = "brown";
+table.style.borderWidth = "10px";
+table.style.padding = "10px 10px";
+
+table.appendChild(table);
+
+
+
+
