74f6cc5af822a3680d77d428dcf2dda5df7a0b9b
Jeff Cook <jecook2692@gmail.com>
2020-04-25 17:27:04 -0400

    adds javascript functions

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,18 +1,1 @@
-// const table = document.getElementById("table");
-// table.style.backgroundColor = "green";
-// table.style.display = "flex";
-// table.style.flexDirection = "column";
-// table.style.alignItems ="center";
-// table.style.marginTop = "130px";
-// table.style.marginLeft = "100px"
-// table.style.width = "80vw";
-// table.style.height = "35vw";
-// table.style.borderRadius = "80px";
-// table.style.borderStyle = "solid";
-// table.style.borderColor = "saddlebrown";
-// table.style.borderWidth = "10px";
-// table.style.padding = "10px 10px";
-
-
-
 
