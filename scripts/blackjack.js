ec532806dacbeae7e75dc48829adb3602365a078
Jeff Cook <jecook2692@gmail.com>
2020-04-25 01:25:35 -0400

    adds updates to styles

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,31 +1,18 @@
-const table = document.getElementById("table");
-table.style.backgroundColor = "green";
-table.style.display = "flex";
-table.style.flexDirection = "column";
-table.style.alignItems ="center";
-table.style.marginTop = "130px";
-table.style.marginLeft = "100px"
-table.style.width = "80vw";
-table.style.height = "35vw";
-table.style.borderRadius = "80px";
-table.style.borderStyle = "solid";
-table.style.borderColor = "saddlebrown";
-table.style.borderWidth = "10px";
-table.style.padding = "10px 10px";
+// const table = document.getElementById("table");
+// table.style.backgroundColor = "green";
+// table.style.display = "flex";
+// table.style.flexDirection = "column";
+// table.style.alignItems ="center";
+// table.style.marginTop = "130px";
+// table.style.marginLeft = "100px"
+// table.style.width = "80vw";
+// table.style.height = "35vw";
+// table.style.borderRadius = "80px";
+// table.style.borderStyle = "solid";
+// table.style.borderColor = "saddlebrown";
+// table.style.borderWidth = "10px";
+// table.style.padding = "10px 10px";
 
-// const button = document.getElementsByClassName("buttons");
-// button.style.marginTop = "250px";
-// button.style.color = "245, 184, 30";
-
-
-
-// const labels = document.getElementsByTagName("label");
-
-// for (let i = 0; i < labels.length; i++) {
-//     const label = labels[i];
-//     label.style.color = "white";
-//     label.style.marginTop = "20px";
-// }
 
 
 
