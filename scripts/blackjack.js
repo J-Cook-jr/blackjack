babc094f6d8d0102a41aa058d808aa47f396d8dd
Jeff Cook <jecook2692@gmail.com>
2020-04-26 16:49:36 -0400

    adds stand button function to js

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -35,5 +35,40 @@ $(function jackDiamond() {
         }
     });
     
+    //creates "stand-button" click function that runs when player clicks "stand"
+    $('#stand-button').click(function () {
+        //creates a "while" loop that deals cards to the dealer hand while dealers points are less than 17
+        //the "calculatePoints" function is run to check the dealers current point amount
+        while (calculatePoints(dealerHand) < 17) {
+            dealACard(dealerHand, '#dealer-hand');
+        }
+        // creates an "if" statement to check for bust
+        if (calculatePoints(dealerHand) > 21) {
+            // if dealer busts the HTML "messages" element will be changed to "Dealer busts! You win!"
+            //the message will also display on the screen
+            $('#messages').text('Dealer busts! You win!');
+        } else if (calculatePoints(playerHand) > 21) {
+            // message displays when the player busts
+            $('#messages').text('You bust!');
+        } else {
+            // using "calculatePoints" function, the "else" statement determines the winner after calculating the dealer and players points
+            var dealerPoints = calculatePoints(dealerHand);
+            var playerPoints = calculatePoints(playerHand);
+            var message;
+            if (dealerPoints > playerPoints) {
+                message = 'You lose!';
+            } else if (dealerPoints < playerPoints) {
+                message = 'You win!';
+            } else {
+                message = 'Push.'
+            }
+            $('#messages').text(message);
+        }
+        // "gameover" function will be called after the calculations and message display
+        gameOver();
+    });
+
+   
+    
 
 }); 
