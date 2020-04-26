eb12020625d9dda312caaed8cfffd52e9ccb98ee
Jeff Cook <jecook2692@gmail.com>
2020-04-26 15:43:09 -0400

    adds jquery, lodash, bootstrap and js function

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,1 +1,27 @@
+//creates function "jackDiamond" that will manipulate "index.HTML" file
+$(function jackDiamond() {
+    //creates "play-again" element that is hidden when the game starts
+    $('#play-again').hide();
+    //creates variables that will be used to define elements in the html file
+    var deck, dealerHand, playerHand;
+    //calls a function to start a new game
+    setupNewGame();
+    //calls a function to update the score
+    updateScoreDisplay();
+     
+    //creates a function that will deal two cards to the player and dealer when the "deal" button is clicked
+    $('#deal-button').click(function () {
 
+        dealACard(playerHand, '#player-hand');
+        dealACard(dealerHand, '#dealer-hand');
+        dealACard(playerHand, '#player-hand');
+        dealACard(dealerHand, '#dealer-hand');
+
+        console.log('playerHand', playerHand);
+        console.log('dealerHand', dealerHand);
+        //deal button will be hidden after it is clicked
+        $('#deal-button').hide();
+    });
+    
+
+}); 
